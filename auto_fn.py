fe1f172bf578d4abc3f983b8e91d781553bbffb2
maciejrum <maciej.rumian2@gmail.com>
2022-10-31 20:05:48 +0100

    basic auto inplementation

---
+++ auto_fn.py
@@ -0,0 +1,52 @@
+# auto - brand, speed, max_speed, engine
+
+bmw = {'brand': 'BMW',
+       'speed': 0,
+       'max_speed': 250,
+       'engine': False}
+
+
+def start_engine(car):
+    if not car['engine']:
+        car['engine'] = True
+        print('Engine started')
+    else:
+        print('Engine already started')
+
+
+def stop_engine(car):
+    car['engine'] = False
+    print('Engine stopped')
+
+
+def accelerate(car, speed):
+    if car['engine']:
+        car['speed'] = min(car['speed'] + speed, car['max_speed'])
+        print('Speed: {}'.format(car['speed']))
+    else:
+        print('Engine is not started')
+
+
+def decelerate(car, speed):
+    car['speed'] = max(car['speed'] - speed, 0)
+    print('Speed: {}'.format(car['speed']))
+
+
+def auto_factory(brand, max_speed):
+    return {
+        'brand': brand,
+        'speed': 0,
+        'max_speed': max_speed,
+        'engine': False
+    }
+
+
+accelerate(bmw, 100)
+start_engine(bmw)
+accelerate(bmw, 100)
+accelerate(bmw, 100)
+accelerate(bmw, 100)
+decelerate(bmw, 100)
+decelerate(bmw, 100)
+decelerate(bmw, 500)
+stop_engine(bmw)
